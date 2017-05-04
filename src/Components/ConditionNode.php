62d6ccf533015018f48ad42d93a4b58cf9bbab87
David Anderson <andersonda12@students.ecu.edu>
2017-05-04 14:38:01 -0400

    initial work (mostly skeletons) on new classes for representing and parsing SQL conditions

--- src/Components/ConditionNode.php
+++ src/Components/ConditionNode.php
@@ -8,6 +8,8 @@
 
 namespace PhpMyAdmin\SqlParser\Components;
 
+use PhpMyAdmin\SqlParser\TokensList;
+
 /**
  * Class ConditionNode
  * @package PhpMyAdmin\SqlParser\Components
@@ -25,4 +27,94 @@ class ConditionNode
         $this->left = null;
         $this->right = null;
     }
+}
+
+class SimpleCondition
+{
+    /**
+     * @var TokensList
+     */
+    public $list;
+
+    public function __construct($tokens){
+        $this->tokens = $tokens;
+    }
+
+    /**
+     * @return SimpleCondition
+     */
+    public function parse(){
+        //TODO: implement parsing
+    }
+}
+
+/**
+ * represents condition of the form expr0 BETWEEN expr1 AND expr2
+ *
+ * Class BetweenCondition
+ * @package PhpMyAdmin\SqlParser\Components
+ */
+class BetweenCondition extends SimpleCondition
+{
+    /**
+     * is this BetweenCondition negated?
+     *
+     * @var bool
+     */
+    public $not = false;
+
+    /**
+     * expression before BETWEEN keyword
+     *
+     * @var string
+     */
+    public $expr = "";
+
+    /**
+     * lower bounds of the BETWEEN statement
+     *
+     * @var string
+     */
+    public $lowerBounds = "";
+
+    /**
+     * upper bounds of the BETWEEN statement
+     *
+     * @var string
+     */
+    public $upperBounds = "";
+}
+
+/**
+ * represents conditions of the form expr IS [NOT] NULL
+ *
+ * Class NullCondition
+ * @package PhpMyAdmin\SqlParser\Components
+ */
+class NullCondition extends SimpleCondition
+{
+    /**
+     * is this NullCondition negated?
+     *
+     * @var bool
+     */
+    public $not = false;
+
+    /**
+     * The expression checked for null
+     *
+     * @var string
+     */
+    public $expr = "";
+}
+
+/**
+ * represents simple conditions such as expr0 = expr1, expr0 < expr1 < expr2, etc.
+ *
+ * Class ComparisonCondition
+ * @package PhpMyAdmin\SqlParser\Components
+ */
+class ComparisonCondition extends SimpleCondition
+{
+    //TODO implement comparison condtions
 }
